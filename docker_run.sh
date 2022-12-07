85a61ae067ba4d11975fbce5140141bf2b8aa964
DaHoon Park <pdh930105@gmail.com>
2022-12-07 09:38:02 +0900

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,5 @@
 docker build . --tag computer_architecture
 docker rm -f ca_container
-docker run -itd --rm --ipc host -p 12341:12341 -p 8888:8888 -v ${PWD}/result/:/computer_architecture/result/ --name ca_container computer_architecture /bin/bash
+docker run -itd --rm --ipc host -p 12341:12341 -p 8888:8888 --name ca_container computer_architecture /bin/bash
 docker exec -it ca_container /bin/bash
 
